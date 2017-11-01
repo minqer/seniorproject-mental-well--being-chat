0769bf685878318f3b1d27ae3184265b7d9f58be
admin <Minq@admins-MacBook-Pro-2.local>
2017-11-01 15:57:06 +0700

    add data to redis

--- app/Http/Controllers/API/APIController.php
+++ app/Http/Controllers/API/APIController.php
@@ -5,13 +5,26 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Predis\Client;
 
 class APIController extends Controller
 {
      public function send($sendId,$receiveId,$msg)
      {
          
-         DB::insert('insert into datalog (sender, receive, textdata) values (?, ?, ?)', [$sendId,$receiveId,$msg]);
+         //DB::insert('insert into datalog (sender, receive, textdata) values (?, ?, ?)', [$sendId,$receiveId,$msg]);
+         
+         if($sendId > $receiveId){
+             $first = $receiveId;
+             $last = $sendId;
+         }
+         else{
+             $first = $sendId;
+             $last = $receiveId;
+         }
+         
+         $client = new \Predis\Client();
+         $client ->lpush("$first:$last", "$sendId:$msg");
          return response()->json(array(
                                        'result' => 'ok',
                                        'message' => 'message is sent',
@@ -22,11 +35,35 @@ class APIController extends Controller
     public function get($sendId,$receiveId,$lastn)
     {
         
-        $message = DB::select('SELECT * FROM `datalog` where (sender =? and receive =?) or (sender =? and receive =?) order by id DESC limit ?', [$sendId,$receiveId,$receiveId,$sendId,$lastn]);
+       // $message = DB::select('SELECT * FROM `datalog` where (sender =? and receive =?) or (sender =? and receive =?) order by id DESC limit ?', [$sendId,$receiveId,$receiveId,$sendId,$lastn]);
+        
+        if($sendId > $receiveId){
+            $first = $receiveId;
+            $last = $sendId;
+        }
+        else{
+            $first = $sendId;
+            $last = $receiveId;
+        }
+        
+        $client = new \Predis\Client();
+        $messages = $client ->lrange("$first:$last", 0,$lastn);
+        
+        $newMessages = array();
+        foreach ($messages as $message) {
+            $tmp = explode(":", $message);
+            $newMessage = array(
+                                'sender' => $tmp[0],
+                                'receive' => $tmp[0] == $sendId ? $receiveId : $sendId,
+                                'textdata' => $tmp[1],
+                                );
+            
+            $newMessages[] = $newMessage;
+        }
         return response()->json(array(
                                       'result' => 'ok',
                                       'message' => '',
-                                      'data' => array_reverse($message)
+                                      'data' => array_reverse($newMessages)
                                       ))->header('Access-Control-Allow-Origin', '*');;
     }
 
